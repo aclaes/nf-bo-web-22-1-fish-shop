91f820edac48169f348b3c9c64f5a494c35cb2c9
Alexander Claes <hello@aclaes.com>
2022-08-02 16:53:23 +0200

    Add routes and links

--- pages/categories.js
+++ pages/categories.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Categories() {
   return (
@@ -7,6 +8,19 @@ export default function Categories() {
         <title>Alle Kategorien</title>
       </Head>
       <h1>Kategorien</h1>
+      <p>Liste aller Kategorien</p>
+      <ul>
+        <li>
+          <Link href="/category/meerwasser">
+            <a>Meerwasser</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/category/muscheln">
+            <a>Muscheln</a>
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import StyledHeader from './ui/StyledHeader';
 import StyledNavigation from './ui/StyledNavigation';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <StyledHeader>
       <StyledNavigation>
-        <a>Startseite</a>
-        <a>Produkte</a>
-        <a>Kategorien</a>
+        <Link href="/">
+          <a>Startseite</a>
+        </Link>
+        <Link href="/products">
+          <a>Produkte</a>
+        </Link>
+        <Link href="/categories">
+          <a>Kategorien</a>
+        </Link>
       </StyledNavigation>
     </StyledHeader>
   );

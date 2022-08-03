4544d4f42e6a9b5b6c1ac2207a687d24e1108bfc
Ryan Ceres <c.nizz@web.de>
2022-08-03 11:41:40 +0200

    Add static site generation with dummy data

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -1,8 +1,36 @@
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import {
+  getAllCategories,
+  getCategoryById,
+} from "../../src/services/categoriesService";
 
-export default function Category() {
-  const router = useRouter();
-  const { id } = router.query;
+export function getStaticPaths() {
+  const categories = getAllCategories();
+  return {
+    paths: categories.map((category) => ({ params: { id: category.id } })),
+    fallback: false,
+  };
+}
 
-  return <h1>Kategorie: {id}</h1>;
+export function getStaticProps(context) {
+  const category = getCategoryById(context.params.id);
+  return {
+    props: { category },
+  };
+}
+
+export default function Category({ category }) {
+  const { id, name, description } = category;
+
+  return (
+    <>
+      <Head>
+        <title>Kategorie: {name}</title>
+      </Head>
+      <h1>Kategorie: {name}</h1>
+      <p>ID: {id}</p>
+      <h2>Beschreibung</h2>
+      <p>{description}</p>
+    </>
+  );
 }
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,8 +1,44 @@
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import {
+  getAllProducts,
+  getProductById,
+} from "../../src/services/productsService";
 
-export default function Product() {
-  const router = useRouter();
-  const { id } = router.query;
+export function getStaticPaths() {
+  const products = getAllProducts();
 
-  return <h1>Produkt: {id}</h1>;
+  return {
+    paths: products.map((product) => ({ params: { id: product.id } })),
+    fallback: false,
+  };
+}
+
+export function getStaticProps(context) {
+  const product = getProductById(context.params.id);
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export default function Product({ product }) {
+  const { id, name, description, price, category } = product;
+
+  return (
+    <>
+      <Head>
+        <title>Produkt: {name}</title>
+      </Head>
+      <h1>Produkt: {name}</h1>
+      <p>ID: {id}</p>
+      <h2>Beschreibung</h2>
+      <p>{description}</p>
+      <h2>Preis</h2>
+      <p>{price}</p>
+      <h2>Kategorie</h2>
+      <p>{category}</p>
+    </>
+  );
 }
